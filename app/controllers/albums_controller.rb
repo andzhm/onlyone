77420633386be53863dd3a8568b29d4546ed7440
ZhmAA <azhm@ukr.net>
2015-03-22 20:34:16 +0200

    Add controller actions

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,15 +5,42 @@ class AlbumsController < ApplicationController
   end
 
   def show
+    @album = Album.find(params[:id])
   end
 
   def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+    if @album.update_attributes(album_params)
+      redirect_to(:action => 'show', :id => @album.id)
+    else
+      render('index')
+    end
   end
 
   def delete
+    @album = Album.find(params[:id])
+  end
+
+  def destroy
+    Album.find(params[:id]).destroy
+    redirect_to(:action => 'index')
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'stat_pages#home'
 
   resources :albums
+  get 'albums/:id/delete' => 'albums#delete', :as => :categories_delete
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
