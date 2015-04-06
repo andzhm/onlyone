0309ff2d317d08fffa35923802c0c330e77e22ce
Eugene Stroganov <estroganov@sphereconsultinginc.com>
2015-04-05 23:07:17 -0500

    gitignore file updated, ruby-version file added, photo seeds commented during migration error - need review

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@
 first = Album.create(name: 'first', description: 'lalalalla')
 second = Album.create(name: 'second', description: 'lalalalla')
 
-first_photo = Photo.create(name: 'firstphoto', description: 'lalalalla', album_id: first.id)
-second_photo = Photo.create(name: 'firstphoto', description: 'lalalalla', album_id: first.id)
-third = Photo.create(name: 'thirdphoto', description: 'lalalalla', album_id: second.id)
+# first_photo = Photo.create(name: 'firstphoto', description: 'lalalalla', album_id: first.id)
+# second_photo = Photo.create(name: 'firstphoto', description: 'lalalalla', album_id: first.id)
+# third = Photo.create(name: 'thirdphoto', description: 'lalalalla', album_id: second.id)
