a110c3366ab00f306b45b52266740644c948f4d6
ZhmAA <azhm@ukr.net>
2015-04-09 21:38:28 +0300

    day end 0904

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+admin_main = Admin.create(email: 'name@name.com', password: '123456789', password_confirmation: '123456789')
 
-first = Album.create(name: 'first', description: 'lalalalla')
-second = Album.create(name: 'second', description: 'lalalalla')
 
-# first_photo = Photo.create(name: 'firstphoto', description: 'lalalalla', album_id: first.id)
-# second_photo = Photo.create(name: 'firstphoto', description: 'lalalalla', album_id: first.id)
-# third = Photo.create(name: 'thirdphoto', description: 'lalalalla', album_id: second.id)
+first = Album.create(name: 'first', description: 'testing', image: '/images/no-image.png')
+second = Album.create(name: 'second', description: 'testing', image: '/images/no-image.png')
+third = Album.create(name: 'third', description: 'testing', image: '/images/no-image.png')
+four = Album.create(name: 'four', description: 'testing', image: '/images/no-image.png')
+
+
+
+first_photo1 = Photo.create(photo_name: 'first photo1', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: first.id)
+first_photo2 = Photo.create(photo_name: 'first photo2', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: first.id)
+first_photo3 = Photo.create(photo_name: 'first photo3', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: first.id)
+first_photo4 = Photo.create(photo_name: 'first photo4', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: first.id)
+first_photo5 = Photo.create(photo_name: 'first photo5', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: first.id)
+
+second_photo1 = Photo.create(photo_name: 'second photo1', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: second.id)
+second_photo2 = Photo.create(photo_name: 'second photo2', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: second.id)
+
+third_photo1 = Photo.create(photo_name: 'third photo1', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: third.id)
+third_photo2 = Photo.create(photo_name: 'third photo2', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: third.id)
+
+four_photo1 = Photo.create(photo_name: 'four photo1', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: four.id)
+four_photo2 = Photo.create(photo_name: 'four photo2', photo_description: 'testing', photoimage: '/images/no-image.png', album_id: four.id)
