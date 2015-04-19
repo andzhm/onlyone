a9096288955cb94390f170622a5562e0ef081017
ZhmAA <azhm@ukr.net>
2015-04-19 21:47:38 +0300

    add scheme.rb to gitignore

--- app/controllers/stat_pages_controller.rb
+++ app/controllers/stat_pages_controller.rb
@@ -2,7 +2,7 @@ class StatPagesController < ApplicationController
 
 	def home
 		@albums         = Album.order(created_at: :desc).limit(4)
-    @latest_news    = Novelty.order(created_at: :desc).limit(5)
+    	@latest_news    = Novelty.order(created_at: :desc).limit(5)
 	end
 	
 	def signup
